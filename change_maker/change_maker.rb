ccb5472293c40986e270229a1bc3605cb75ee3ff
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-11-22 19:00:32 -0600

    I see what I'm doing wrong: its not taking coin order into account

--- change_maker/change_maker.rb
+++ change_maker/change_maker.rb
@@ -1,14 +1,18 @@
 class ChangeMaker
 
-  def ways_to_make_change(number)
+  def ways_to_make_change(number, tracker=[])
     ways = 0
-
     coins.each do |coin|
+      new_tracker = tracker + [coin]
       target = number - coin
-      if target > 0
-        ways += ways_to_make_change(target)
+    puts "[ #{new_tracker.join(', ')} ] = #{new_tracker.reduce(&:+)}"
+    puts "number: #{number} - #{coin} = #{target}"
+
+      if coin <= target && target > 0
+        ways += ways_to_make_change(target, new_tracker)
       elsif target == 0
-        ways = 1
+        ways += 1
+    puts "[ #{new_tracker.join(', ')} ] = #{new_tracker.reduce(&:+)}"
       end
     end
 
--- spec/change_maker_spec.rb
+++ spec/change_maker_spec.rb
@@ -14,5 +14,11 @@ RSpec.describe ChangeMaker do
         expect(subject.ways_to_make_change(5)).to eq(2)
       end
     end
+
+    context 'when making change for 11 cents' do
+      it 'finds three ways to make change' do
+        expect(subject.ways_to_make_change(11)).to eq(4)
+      end
+    end
   end
 end
