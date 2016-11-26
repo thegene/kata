975fb0792e6d2e193d158029503d31ffd3ce2d35
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-11-25 19:21:18 -0600

    Working except failing in seed 21344

--- change_maker/change_maker.rb
+++ change_maker/change_maker.rb
@@ -1,25 +1,27 @@
 class ChangeMaker
 
-  def ways_to_make_change(number, tracker=[])
+  def ways_to_make_change(number)
+    find_ways_in(number, COINS)
+  end
+
+  def find_ways_in(number, coins)
     ways = 0
     coins.each do |coin|
-      new_tracker = tracker + [coin]
       target = number - coin
-    puts "[ #{new_tracker.join(', ')} ] = #{new_tracker.reduce(&:+)}"
-    puts "number: #{number} - #{coin} = #{target}"
 
-      if coin <= target && target > 0
-        ways += ways_to_make_change(target, new_tracker)
-      elsif target == 0
+      if target == 0 # we're done
         ways += 1
-    puts "[ #{new_tracker.join(', ')} ] = #{new_tracker.reduce(&:+)}"
+
+      elsif target >= coin # we can still try larger coin
+        ways += find_ways_in(target, coins)
+      elsif target > 0 # drop the larger coin
+        coins.shift
+        ways += find_ways_in(target, coins)
       end
     end
-
     return ways
   end
 
-  def coins
-    [25, 10, 5, 1]
-  end
+  COINS = [10, 5, 1]
+
 end
