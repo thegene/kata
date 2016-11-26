173f1a6bf338df5dc915d67c1baf753ec7b1756a
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-11-26 14:11:04 -0600

    Clearer, more useful tests

--- spec/change_maker_spec.rb
+++ spec/change_maker_spec.rb
@@ -1,23 +1,46 @@
 require './change_maker/change_maker'
 RSpec.describe ChangeMaker do
   context 'Given a ChangeMaker' do
-    subject { described_class.new }
+    subject { described_class.new.ways_to_make_change(number) }
 
     context 'when making change for 1 cent' do
+      let(:number) { 1 }
+
       it 'finds one way to make change' do
-        expect(subject.ways_to_make_change(1)).to eq(1)
+        expect(subject.count).to eq(1)
+      end
+
+      it 'is only a penny' do
+        expect(subject).to match_array([[1]])
       end
     end
 
     context 'when making change for 5 cents' do
+      let(:number) { 5 }
+
       it 'finds two ways to make change' do
-        expect(subject.ways_to_make_change(5)).to eq(2)
+        expect(subject.count).to eq(2)
+      end
+
+      it 'returns a nickel or finve pennies' do
+        expect(subject).to match_array([[5], [1, 1, 1, 1, 1]])
       end
     end
 
     context 'when making change for 11 cents' do
+      let(:number) { 11 }
+
       it 'finds three ways to make change' do
-        expect(subject.ways_to_make_change(11)).to eq(4)
+        expect(subject.count).to eq(4)
+      end
+
+      it 'is only a penny' do
+        expect(subject).to match_array([
+          [10, 1],
+          [5, 5, 1],
+          [5, 1, 1, 1, 1, 1, 1],
+          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ])
       end
     end
   end
