38714de2f5b49a6b2205ebb59b6f3b354ac971c5
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-11-25 19:24:54 -0600

    Fixed seed issue but now the quarter breaks it

--- change_maker/change_maker.rb
+++ change_maker/change_maker.rb
@@ -1,7 +1,7 @@
 class ChangeMaker
 
   def ways_to_make_change(number)
-    find_ways_in(number, COINS)
+    find_ways_in(number, [25, 10, 5, 1])
   end
 
   def find_ways_in(number, coins)
@@ -9,11 +9,14 @@ class ChangeMaker
     coins.each do |coin|
       target = number - coin
 
+      puts "N: #{number}, C: #{coin}, T: #{target}, #{coins.join(',')}"
+
       if target == 0 # we're done
         ways += 1
 
       elsif target >= coin # we can still try larger coin
         ways += find_ways_in(target, coins)
+
       elsif target > 0 # drop the larger coin
         coins.shift
         ways += find_ways_in(target, coins)
@@ -21,7 +24,5 @@ class ChangeMaker
     end
     return ways
   end
-
-  COINS = [10, 5, 1]
 
 end
