ff341bc49d32c02bee0aa07c18bacd26dc33e37b
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-12-11 14:46:20 -0600

    Separate combos and combos for coin

--- change_maker/change_maker.rb
+++ change_maker/change_maker.rb
@@ -1,29 +1,36 @@
 class ChangeMaker
+
   def make_change_for(number)
     all_combos_with(number, [25, 10, 5, 1])
   end
 
   def all_combos_with(target, coins)
-    [].tap do |combos|
-      while coin = coins.shift
-        parts = get_parts_for(target, coin)
-        parts.each do |part|
-          remainder = target - sum(part)
+    combos = []
 
-          if remainder == 0
-            combos << part
-          else
+    while coin = coins.shift
+      combos_for_coin = []
 
-            new_parts = all_combos_with(remainder, coins.dup)
-            new_parts.each do |new_part|
-              combos << part + new_part
-            end
+      parts = get_parts_for(target, coin)
+      parts.each do |part|
+        remainder = target - sum(part)
 
+        if remainder == 0
+          combos_for_coin << part
+        else
+
+          new_parts = all_combos_with(remainder, coins.dup)
+          new_parts.each do |new_part|
+            combos_for_coin << part + new_part
           end
 
         end
+
       end
+
+      combos += combos_for_coin
     end
+
+    combos
   end
 
   def get_parts_for(target, coin)
