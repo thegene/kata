e9b8d4c9b8ff9e7d3eb19ea3ee064e255e88b002
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-12-11 15:59:14 -0600

    Now getting the first 3 fib nums

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,5 +1,18 @@
 class Fibonacci
+  attr_accessor :current, :last
+
+  def initialize
+    @current = 0
+    @last = 1
+  end
+
   def find(number)
-    number
+    while number > 0
+      old = self.current
+      self.current = self.current + self.last
+      self.last = old
+      number -= 1
+    end
+    current
   end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,13 +1,14 @@
 require './fibonacci/fibonacci'
 
 RSpec.describe Fibonacci do
-  subject { described_class.new.find(number) }
+  let(:instance) { described_class.new }
+  subject { instance.find(number) }
 
   context 'Given we want the zero-eth fibonacci number' do
     let(:number) { 0 }
 
     it 'returns 0' do
-      expect(subject).to be(0)
+      expect(subject).to eq(0)
     end
   end
 
@@ -15,7 +16,15 @@ RSpec.describe Fibonacci do
     let(:number) { 1 }
 
     it 'returns 1' do
-      expect(subject).to be(1)
+      expect(subject).to eq(1)
+    end
+  end
+
+  context 'Given we want the second' do
+    let(:number) { 2 }
+
+    it 'returns 1' do
+      expect(subject).to eq(1)
     end
   end
 end
