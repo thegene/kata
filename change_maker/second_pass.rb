6c8a99ee723044180da307bd99d3d47b5f49f7d0
Eugene Westbrook <eugenewestbrook@gmail.com>
2017-04-21 13:38:38 -0500

    Solving for 5 cents

--- change_maker/second_pass.rb
+++ change_maker/second_pass.rb
@@ -2,7 +2,7 @@ module ChangeMaker
   class SecondPass
 
     def make_change_for(number)
-      all_paths_for(number)
+      all_paths_for(number, all_coins)
     end
 
     private
@@ -29,13 +29,24 @@ module ChangeMaker
       combos
     end
 
-    def all_paths_for(value)
-      coins = all_coins
-      while coin = coins.shift
-        combos_for_coin = combinations_for_coin(coin, value)
+    def all_paths_for(value, coins)
+      [].tap do |paths|
+        while coin = coins.shift
+          combos = combinations_for_coin(coin, value)
+          combos.each do |combo|
+            new_value = find_new_value_from_combo(combo, value)
 
-        puts combos_for_coin.inspect
+            if new_value == 0
+              paths << combo
+            end
+          end
+
+        end
       end
+    end
+
+    def find_new_value_from_combo(combo, original_value)
+      original_value - combo.reduce(0, :+)
     end
   end
 end
